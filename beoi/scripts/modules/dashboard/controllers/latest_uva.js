563fcaea97367172d0ddebceb032c22547322c16
Damien Leroy <damien.leroy@be-oi.be>
2015-03-07 22:16:20 +0100

    use db problem list

--- beoi/scripts/modules/dashboard/controllers/latest_uva.js
+++ beoi/scripts/modules/dashboard/controllers/latest_uva.js
@@ -30,88 +30,59 @@ angular
         };
 
 
-        $scope.uva_pids = [964, 2408, 1508, 2070, 3139, 3503, 615, 944, 103, 2548, 288, 4027, 3873, 2671, 2949, 1167, 1895, 3644, 1073];
-        $scope.uva_problems = {}
-
-        $http.get('/api/users-uva-ids', {})
+        $http.get('/api/uva_problems', {})
           .success(function(data){
-            var users = data;
-            var uva_users_ids_csv = _.map(users, function(x){return x["uva_user_id"];}).join();
-            var uva_pids_csv = $scope.uva_pids.join();
-            $http.get('http://uhunt.felix-halim.net/api/subs-pids/'+uva_users_ids_csv+'/'+uva_pids_csv , {})
+
+            $scope.problem_sets = data;
+            $scope.uva_pids = _.pluck(_.flatten(_.pluck(data,'problems')),'uva_pid');
+
+            $http.get('/api/users-uva-ids', {})
               .success(function(data){
+                var users = data;
+                var uva_users_ids_csv = _.pluck(users, 'uva_user_id').join();
+                var uva_pids_csv = $scope.uva_pids.join();
+                $http.get('http://uhunt.felix-halim.net/api/subs-pids/'+uva_users_ids_csv+'/'+uva_pids_csv , {})
+                  .success(function(data){
 
-                $scope.uva_results = _.map(users, function(user) {
-                  var RESULT_NONE = 0, RESULT_TRIED = 1, RESULT_SUCCEEDED = 2;
-                  var uva_user_id = user["uva_user_id"];
-                  var user_submissions = data[uva_user_id]["subs"];
-                  var user_results = _.map($scope.uva_pids, function(pid){
-                    return _.reduce(user_submissions, function(memo, result) {
-                      function compResult(verdictId) {
-                        if (verdictId == 90) return RESULT_SUCCEEDED;
-                        else return RESULT_TRIED;
+                    $scope.uva_results = _.map(users, function(user) {
+                      var RESULT_NONE = 0, RESULT_TRIED = 1, RESULT_SUCCEEDED = 2;
+                      var uva_user_id = user["uva_user_id"];
+                      var user_submissions = data[uva_user_id]["subs"];
+                      var user_results_list = _.map($scope.uva_pids, function(pid){
+                        return _.reduce(user_submissions, function(memo, result) {
+                          function compResult(verdictId) {
+                            if (verdictId == 90) return RESULT_SUCCEEDED;
+                            else return RESULT_TRIED;
+                          }
+                          return _.max([memo, (result[1] == pid)? compResult(result[2]) : RESULT_NONE]);
+                        }, RESULT_NONE);
+                      });
+                      var user_results = _.object($scope.uva_pids, user_results_list);
+                      return {
+                        name: user["name"],
+                        uva_user_id: uva_user_id,
+                        uva_problem_results: user_results
                       }
-                      return _.max([memo, (result[1] == pid)? compResult(result[2]) : RESULT_NONE]);
-                    }, RESULT_NONE);
-                  });
-                  return {
-                    name: user["name"],
-                    uva_user_id: uva_user_id,
-                    uva_problem_results: user_results
-                  }
-                });
+                    });
+                  });   
+
               });
-          });
 
-        _.each($scope.uva_pids, function(pid){
-          $http.get('http://uhunt.felix-halim.net/api/p/id/'+pid)
-            .success(function(data){
-              var section = Math.floor(data.num/100);
-              data.ext_problem_url1 = 'http://uva.onlinejudge.org/external/'+section+'/'+data.num+'.html';
-              data.ext_problem_url2 = 'http://acm.uva.es/p/v'+section+'/'+data.num+'.html';
-              data.problem_url = 'http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem='+data.pid;
-              data.submit_url = 'http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=submit_problem&problemid='+data.pid+'&category=24';
-              data.stats_url = 'http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=problem_stats&problemid='+data.pid+'&category=24';
-              $scope.uva_problems[pid] = data;
+            $scope.uva_problems = {};
+            _.each($scope.uva_pids, function(pid){
+              $http.get('http://uhunt.felix-halim.net/api/p/id/'+pid)
+                .success(function(data){
+                  var section = Math.floor(data.num/100);
+                  data.ext_problem_url1 = 'http://uva.onlinejudge.org/external/'+section+'/'+data.num+'.html';
+                  data.ext_problem_url2 = 'http://acm.uva.es/p/v'+section+'/'+data.num+'.html';
+                  data.problem_url = 'http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem='+data.pid;
+                  data.submit_url = 'http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=submit_problem&problemid='+data.pid+'&category=24';
+                  data.stats_url = 'http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=problem_stats&problemid='+data.pid+'&category=24';
+                  $scope.uva_problems[pid] = data;
+                });
             });
-        });
 
-
-
-
-
-
-
-        // $scope.submissions = [];
-        // var maxcount = 10;
-
-        // _.each(uva_user_ids, function(user_id){
-        //   $http.get('http://uhunt.felix-halim.net/api/subs-user-last/'+user_id+'/'+maxcount, {})
-        //   .success(function(data){
-        //     if (data['subs'].length) {
-        //       var previous = $scope.submissions;
-        //       var new_values = _.map(data['subs'], function(sub){
-        //         return {
-        //           name: data['name'],
-        //           uname: data['uname'],
-        //           sub_id: sub[0],
-        //           prob_id: sub[1],
-        //           verdict_id: sub[2],
-        //           runtime: sub[3],
-        //           sub_time: sub[4]*1000,
-        //           lang_id: sub[5],
-        //           sub_rank: sub[6]
-        //         }
-        //       })
-        //       previous = previous.concat(new_values);
-        //       previous = _.sortBy(previous, function(sub){
-        //         return -sub['sub_time'];
-        //       });
-        //       $scope.submissions = previous.slice(0,maxcount);
-        //     };
-        //   });
-        // });
-
+          });
 
       }
     ]
